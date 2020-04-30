ed3a7af7013d8dd1768325a0e4f6ccaaa3c179f0
manwar16 <mdmanwarcse16@gmail .com>
2020-05-01 00:22:15 +0600

    let & const

---
+++ letconst.js
@@ -0,0 +1,8 @@
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12)
+console.log(numbers);
+
+let patientName = 'Rahim chacha'
+patientName = 'karim chacha'
+console.log(patientName);
---
+++ tamplate.js
@@ -0,0 +1,7 @@
+const firstName =' Md';
+const lastName = 'Mono';
+const fullName = firstName + " " + lastName;
+console.log(fullName)
+
+const fullName1 = `${"mono"} ${"mohon"} is a good boy`;
+console.log(fullName1);
